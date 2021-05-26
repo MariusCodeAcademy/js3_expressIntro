a1f3996d9318722c58f72013a4aa374faccc756d
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-26 13:46:55 +0300

    people data, staticPath

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@
 const express = require('express');
 const path = require('path');
 
+const { people } = require('./js/people');
+
 // sukuriam express app objekta
 const app = express();
 
+// current paths
+const htmlPath = path.join(__dirname, 'html');
 const indexPath = path.join(__dirname, 'html', 'index.html');
 const aboutPath = path.join(__dirname, 'html', 'about.html');
 // console.log(' indexPath', indexPath);
 
-// main route
-app.get('/', (req, res) => res.sendFile(indexPath));
-app.get('/about', (req, res) => res.sendFile(aboutPath));
+// routes
+// app.get('/', (req, res) => res.sendFile(indexPath));
+// app.get('/about', (req, res) => res.sendFile(aboutPath));
+
+// kai turim papke kurios failus norim pasiekti is narsykles pagal pavadinimas
+// nustatom static papke.
+// app.use(express.static(htmlPath));
 
 // paleidzia serveri ir klausosi http ir kt requestu nurodytu portu
 app.listen(3000, () => console.log('server is running'));
