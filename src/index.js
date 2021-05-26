0296508a4943c263f6932f720521d472c676c689
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-26 13:33:49 +0300

    about page manual route added

--- src/index.js
+++ src/index.js
@@ -6,15 +6,12 @@ const path = require('path');
 const app = express();
 
 const indexPath = path.join(__dirname, 'html', 'index.html');
+const aboutPath = path.join(__dirname, 'html', 'about.html');
 // console.log(' indexPath', indexPath);
 
 // main route
-app.get('/', (req, res) => {
-  //   res.send('<h1>Hello world from Express</h1>');
-  res.sendFile(indexPath);
-});
-
-console.log('Works');
+app.get('/', (req, res) => res.sendFile(indexPath));
+app.get('/about', (req, res) => res.sendFile(aboutPath));
 
 // paleidzia serveri ir klausosi http ir kt requestu nurodytu portu
 app.listen(3000, () => console.log('server is running'));
