5b0dfdedb16022593a0ad6f2aaec52c323d9941e
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-26 13:25:10 +0300

    res.sendFile, path log

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 // isitraukiam express is npm
 const express = require('express');
+const path = require('path');
 
 // sukuriam express app objekta
 const app = express();
 
+const indexPath = path.join(__dirname, 'html', 'index.html');
+// console.log(' indexPath', indexPath);
+
 // main route
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world from Express</h1>');
+  //   res.send('<h1>Hello world from Express</h1>');
+  res.sendFile(indexPath);
 });
 
+console.log('Works');
+
+// paleidzia serveri ir klausosi http ir kt requestu nurodytu portu
 app.listen(3000, () => console.log('server is running'));
