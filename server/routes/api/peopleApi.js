27f6e5e460e46f1a1c6f6be718e01ecc663d8f72
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-27 10:03:24 +0300

    create user endpont POST

--- server/routes/api/peopleApi.js
+++ server/routes/api/peopleApi.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 const people = require('../../js/peopleData');
 
-// our api
+// get all people Endpoint
 router.get('/', (req, res) => {
   // grazinam json
   res.json(people);
 });
-// get one people
+// get one people Endpoint
 router.get('/:id', (req, res) => {
   const paramId = req.params.id;
 
@@ -20,6 +20,13 @@ router.get('/:id', (req, res) => {
 
   // grazinam json
   res.json(found);
+});
+
+// Create one people Endpoint
+// gauti duomenis is vartojo formos arba json pavidalu ir sukuri nauja partotoja tarp savo people
+router.post('/', (req, res) => {
+  console.log(' req.body', req.body);
+  res.send(req.body);
 });
 
 module.exports = router;
