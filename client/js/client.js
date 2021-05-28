4addd2eb660ec8bf6fddf67d239e334b7840b47d
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-28 08:58:47 +0300

    generate list pradzia

--- client/js/client.js
+++ client/js/client.js
@@ -1,8 +1,13 @@
 console.log('hello client');
 import { fetchGet } from './fetch.js';
+import { generateList } from './generateHtml.js';
 // get people fetch
 document.getElementById('ppl').addEventListener('click', () => {
   fetchGet('/api/people', (data) => console.log(data));
 });
 
-fetchGet('/api/people', (data) => console.log(data));
+fetchGet('/api/people', (data) => {
+  data.forEach((person) => {
+    console.log({ name: person.name, age: person.age });
+  });
+});
